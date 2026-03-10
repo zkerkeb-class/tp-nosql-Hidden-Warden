6d52950cf1db54c20583ba44a638cc8daa161cf7
Hidden-Warden <70717127+Hidden-Warden@users.noreply.github.com>
2026-03-10 11:57:03 +0100

    Setup Docker for MongoDB and Node.js application

--- connect.js
+++ connect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connect = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/pokemon-nosql');
+        await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/pokemon-nosql');
         console.log('Connected to MongoDB');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
